087719e580e543c7a6c97390642dde8a5d122dde
Szymon <szymonngos@gmail.com>
2026-09-14 18:12:25 +0200

    add loading for trip page

--- apps/web/src/app/trip/[id]/page.tsx
+++ apps/web/src/app/trip/[id]/page.tsx
@@ -1,8 +1,4 @@
 import { ViewTripContainer } from '@/features/trip/components/ViewTrip/ViewTripContainer';
-// import { Suspense } from 'react';
-// import { TripLoader } from '@/features/trip/components/TripLoader';
-
-export const revalidate = 60;
 
 const TripPage = ({ params }: { params: { id: number } }) => (
   <div className="h-screen">
